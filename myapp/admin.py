82ac8b0f309b50bf23538fae5e4855f5eed79cd3
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2024-01-27 00:08:48 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import mapPointers
+from .models import mapPointers, myBooking1
 
 # Register your models here.
 admin.site.register(mapPointers)
+admin.site.register(myBooking1)
