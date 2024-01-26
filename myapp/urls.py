b830fddaa691d24c7b12a4aba3d987f42294c396
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2024-01-26 17:13:05 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.register,name='register'),
@@ -8,4 +10,7 @@ urlpatterns = [
     path('display',views.display,name='display'),
     path('provider',views.provider,name='provider'),
     path('need',views.need,name='need'),
-]
+    path('pdashboard',views.pdashboard,name='pdashboard'),
+    path('delLocation/<int:pk>',views.delLocation,name='delLocation'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
