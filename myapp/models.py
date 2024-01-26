1dec8e0041875593f776f9f5a6460a0507e046ff
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2024-01-26 14:29:22 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class mapPointers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    rate = models.IntegerField()
+    photo = models.ImageField()
+
+    def __str__(self):
+        return f'MapPointer {self.id} - User: {self.user.username}'
